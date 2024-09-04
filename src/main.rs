5e5e5fffef153830899973e79b34a74463761590
Limpowitch <timrosendahl2@gmail.com>
2024-09-04 09:01:04 +0200

    added very simple MySQL connection

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,25 @@
 use axum::{response::Html, routing::get, Router};
 use std::fs;
+use sqlx::mysql::MySqlPool;
+use std::env;
+use dotenvy::dotenv;
 
 #[tokio::main]
 async fn main() {
+    //load enviroment variables from .env file
+    dotenv().ok();
+
+    //get the database URL from .env
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+    // Create a connection pool for MySQL
+    let pool = MySqlPool::connect(&database_url).await.expect("Failed to create pool");
+
     // Build our application with two routes
     let app = Router::new()
         .route("/hello", get(hello))
-        .route("/world", get(world));
+        .route("/world", get(world))
+        .route("/db_test", get(move || db_test(pool.clone()))); // Route to test the DB connection
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
@@ -26,4 +39,13 @@ async fn world() -> Html<String> {
     let world_template = fs::read_to_string("templates/world.html")
         .expect("Something went wrong reading the world.html file");
     Html(world_template)
+}
+
+// Function to test database connection
+async fn db_test(pool: MySqlPool) -> Html<String> {
+    // Try to acquire a connection from the pool
+    match pool.acquire().await {
+        Ok(_) => Html("<h1>Database Connection Successful!</h1>".to_string()),
+        Err(_) => Html("<h1>Failed to Connect to Database</h1>".to_string()),
+    }
 }
